be85f328285106b35bb137671739a17a8acd93b3
Sergio Llopis Donate <sllopis@csumb.edu>
2017-02-02 21:21:16 +0000

    Images loaded

--- project1.py
+++ project1.py
@@ -1,2 +1,60 @@
-# this a comment 
-print("HelloWorld")
+from PIL import Image
+
+im = Image.open('images/1.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/2.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/3.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/4.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/5.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/6.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/7.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/8.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+im = Image.open('images/9.png')
+print (im.format)
+print (im.size)
+print (im.mode)
+
+
+#im.new(RGB, (495, 557), color=0)
+
+#px = im.load()
+
+#array
+#loop to check
+#calculate the median
+#info put back into the new image
+#for x range (10):
+    
+
+
